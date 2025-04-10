312645df72cb50cc362380af39aa4fcf5161538d
Joao Ribas <jv.simao10@gmail.com>
2025-04-10 09:43:13 -0300

    Adicionei o menu de seleção do jogo

---
+++ Pedrapapeltesoura.py
@@ -0,0 +1,27 @@
+print("***BEM VINDO***")
+
+
+print("1 - CPU vs CPU")
+print("2 - PLAYER vs CPU")                       #menu para o usuario escolher qual modo de jogo ele deseja
+print("3 - PLAYER vs PLAYER")
+print("4 - Sair")
+
+opcaomenu = int(input("Insira a opção desejada:"))
+
+if opcaomenu == 1:
+    print("Modo CPU VS CPU")
+
+
+
+
+elif opcaomenu == 2:
+    print("Modo PLAYER VS CPU")
+
+
+
+elif opcaomenu == 3:
+    print("Modo PLAYER VS PLAYER")
+
+
+elif opcaomenu == 4:
+    print("Você escolheu sair")
